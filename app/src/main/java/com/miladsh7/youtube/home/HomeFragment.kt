bd258c52cbd5e5bdcbb592887302d20e6e00259c
Miladsh7 <80962004+Miladsh7@users.noreply.github.com>
2022-04-25 17:52:24 +0430

    change the project

--- app/src/main/java/com/miladsh7/youtube/home/HomeFragment.kt
+++ app/src/main/java/com/miladsh7/youtube/home/HomeFragment.kt
@@ -17,7 +17,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(LayoutInflater.from(context),container,false)
         return _binding!!.root
