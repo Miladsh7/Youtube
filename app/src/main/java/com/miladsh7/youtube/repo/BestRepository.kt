dcdbe9a3522e85248bac90ddb1b4770aa4a640c4
Miladsh7 <80962004+Miladsh7@users.noreply.github.com>
2022-04-30 15:32:55 +0430

    change the project

--- app/src/main/java/com/miladsh7/youtube/repo/BestRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.miladsh7.youtube.repo
-
-import com.miladsh7.youtube.model.VideoItem
-import retrofit2.Response
-
-interface BestRepository {
-
-    suspend fun getBestVideos(): List<VideoItem>
-}
--- app/src/main/java/com/miladsh7/youtube/repo/NewRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.miladsh7.youtube.repo
-
-import com.miladsh7.youtube.model.VideoItem
-
-interface NewRepository {
-
-    suspend fun getNewVideos(): List<VideoItem>
-}
